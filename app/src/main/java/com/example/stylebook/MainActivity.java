4b4350fdcdc35227135241819dadf4a6b4e9bd9e
Anne Wu <annewqx@outlook.com>
2019-05-14 22:12:45 +0800

    Homepage and closet button

--- app/src/main/java/com/example/stylebook/MainActivity.java
+++ app/src/main/java/com/example/stylebook/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.stylebook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        ImageButton openCloset=(ImageButton)findViewById(R.id.Button_Closet);
+        openCloset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ClosetActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
